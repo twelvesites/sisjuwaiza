09627d629c9639a7a5375d02fb43391b435d454a
aravaweb <aravindshiju7@gmail.com>
2023-10-12 19:50:46 +0400

    First commit

---
+++ js/slideshow.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+
+function showSlide(n) {
+    const slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    showSlide(slideIndex);
+}
+
+function autoSlide() {
+    changeSlide(1);
+}
+
+showSlide(slideIndex);
+setInterval(autoSlide, 5000); // 5000 milliseconds = 5 seconds
